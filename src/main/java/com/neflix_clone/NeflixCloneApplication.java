d806bb29a242b9844dff1a98ae7a5cf6e97dd9c3
Myo Thu Aung <myothuaung@blue-stone.net>
2025-09-23 14:40:28 +0630

    [ADD] required depenency and file

---
+++ src/main/java/com/neflix_clone/NeflixCloneApplication.java
@@ -0,0 +1,13 @@
+package com.neflix_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeflixCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeflixCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neflix_clone/NeflixCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neflix_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeflixCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
